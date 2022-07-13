bb1acd34bea79b51c555b46e2b9b15943362ba12
Kabir Singh <sahai.kabir1@gmail.com>
2022-07-12 17:29:36 -0700

    UI Tests

---
+++ memeMakerUITests/memeMakerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  memeMakerUITestsLaunchTests.swift
+//  memeMakerUITests
+//
+//  Created by Kabir Singh on 7/12/22.
+//
+
+import XCTest
+
+class memeMakerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
